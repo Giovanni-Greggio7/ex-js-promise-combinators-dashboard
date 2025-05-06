5ec0f420ed8e67dfaf4a5756b0233c314f23dc18
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-06 17:38:10 +0200

    Start exercise

---
+++ main.js
@@ -0,0 +1,40 @@
+// In questo esercizio, utilizzerai Promise.all() per creare la funzione getDashboardData(query), che accetta una città come input e recupera simultaneamente:
+// Nome completo della città e paese da  /destinations?search=[query]
+// (result.name, result.country, nelle nuove proprietà city e country).
+// Il meteo attuale da /weathers?search={query}
+// (result.temperature e result.weather_description nella nuove proprietà temperature e weather).
+// Il nome dell’aeroporto principale da /airports?search={query}
+// (result.name nella nuova proprietà airport).
+// Utilizzerai Promise.all() per eseguire queste richieste in parallelo e poi restituirai un oggetto con i dati aggregati.
+
+
+// Attenzione: le chiamate sono delle ricerche e ritornano un’array ciascuna, di cui devi prendere il primo risultato (il primo elemento).
+// Note del docente
+// Scrivi la funzione getDashboardData(query), che deve:
+// Essere asincrona (async).
+// Utilizzare Promise.all() per eseguire più richieste in parallelo.
+// Restituire una Promise che risolve un oggetto contenente i dati aggregati.
+// Stampare i dati in console in un messaggio ben formattato.
+// Testa la funzione con la query "london
+
+async function fetchData(url){
+    const response = await fetch(url)
+    const obj = await response.json()
+    return obj
+}
+
+async function getDashboardData(query){
+    const city = await fetchData(`https://freetestapi.com/api/v1/destinations?search=${query}`)
+    return city
+}
+
+(async() => {
+    try{
+        const result = await getDashboardData('paris')
+        console.log('Risultato', result)
+    }catch(error){
+        console.error(error)
+    }finally{
+        console.log('Codice eseguito correttamente')
+    }
+})
